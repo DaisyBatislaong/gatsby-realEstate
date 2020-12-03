5aafb4092466843c1d93d3d16da6d32a7ba2e9f6
DaisyBatislaong <dtechimperium@gmail.com>
2020-12-03 12:10:13 +0800

    assemble plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,22 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `c2jlpkowjo08`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
